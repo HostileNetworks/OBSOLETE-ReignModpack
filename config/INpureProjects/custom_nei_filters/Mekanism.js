8875d6c53f695bb87afeaf71f39d201509b2b6ec
CosmicDan <dconnolly.au@gmail.com>
2017-04-01 05:30:25 +1100

    Unhide Mekanism gas tanks

--- config/INpureProjects/custom_nei_filters/Mekanism.js
+++ config/INpureProjects/custom_nei_filters/Mekanism.js
@@ -1,5 +1,4 @@
 if (FML.isModLoaded("Mekanism")) {
-    NEI.override("Mekanism:GasTank", [100]);
     NEI.override("Mekanism:*PlasticBlock", [0]);
     NEI.override("Mekanism:Balloon", [0]);
     NEI.override("Mekanism:GlowPanel", [0]);
